6af8365db7a24dcc62bc8b049f0262119f4d7361
Thomas Cousin <thomas.cousin@me.com>
2021-05-30 21:56:39 +0000

    Additon of mqtt code

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class HassInputSelect{
 		this.mqtt_clientid = config.mqtt.clientid || this.createClientId();
 		this.mqtt_username = config.mqtt.username || 'hass-input-select';
 		this.mqtt_password = config.mqtt.password || 'password';
+		this.mqtt_topic = config.mqtt.topic || 'hass-input-select';
 		this.mqtt_options = {
 			keepalive: 10,
 			clientId: this.mqtt_clientId,
@@ -39,6 +40,51 @@ class HassInputSelect{
 			password: this.mqtt_password,
 			rejectUnauthorized: false
 		};
+
+		this.setupMQTT();
+		this.setupHapServices();
+	}
+
+	setupHapServices() {
+		this.informationService = new Service.AccessoryInformation();
+		this.informationService
+			.setCharacteristic(Characteristic.Manufacturer, "Tom")
+			.setCharacteristic(Characteristic.Model, "HomeAssistant-Input-Select")
+			.setCharacteristic(Characteristic.SerialNumber, "HAIS-" + this.name)
+			.setCharacteristic(
+				Characteristic.FirmwareRevision,
+				require('./package.json').version
+			);
+
+	}
+
+	setupMQTT() {
+                this.mqtt_client = mqtt.connect(this.mqtt_url, this.mqtt_options);
+                this.mqtt_client.on('error', function (err) {
+                        this.log('MQTT Error: ' + err);
+                }.bind(this));
+                this.mqtt_client.on('message', this.handleMqttMessage.bind(this));
+                this.mqtt_client.on('connect', this.handleMqttConnected.bind(this));
+        }
+
+	handleMqttConnected() {
+		this.log('MQTT Connected');
+		this.mqtt_client.subscribe(this.mqtt_topic, function (err) {
+			if (err) {
+				this.log('MQTT Sensor Subscription error:' + err);
+			} else {
+				this.log('MQTT Subscribed');
+			}
+		}.bind(this));
+	}
+
+	handleMqttMessage(topic, message) {
+		this.log("MQTT receieved, Topic: " + topic + " message: " + message);
+		if (topic == this.mqtt_sensor_topic) {
+
+		} else {
+			this.log('MQTT Message error topic: ' + topic + ' message: ' + message);
+		}
 	}
 
 	getServices() {
